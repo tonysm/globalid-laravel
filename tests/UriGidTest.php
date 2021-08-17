020c2426e30c4595191882dfc80d1da56bf35644
Tony Messias <tonysm@hey.com>
2021-08-16 21:43:32 -0300

    Adds first URI GID test

---
+++ tests/UriGidTest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Tonysm\GlobalId\Tests;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UriGidTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function parsed()
+    {
+    }
+
+    /** @test */
+    public function returns_invalid_gid_when_not_checking()
+    {
+    }
+
+    /** @test */
+    public function create()
+    {
+    }
+
+    /** @test */
+    public function build()
+    {
+    }
+
+    /** @test */
+    public function build_with_wrong_ordered_array_creates_wrong_ordered_gid()
+    {
+    }
+
+    /** @test */
+    public function to_string()
+    {
+    }
+
+    /** @test */
+    public function equality()
+    {
+    }
+
+    /** @test */
+    public function encodes_alphanumeric()
+    {
+    }
+
+    /** @test */
+    public function encodes_non_alphanumberic()
+    {
+    }
+
+    /** @test */
+    public function decodes_alphanumeric()
+    {
+    }
+
+    /** @test */
+    public function decodes_non_alphanumerics()
+    {
+    }
+
+    /** @test */
+    public function invalid_gids()
+    {
+    }
+
+    /** @test */
+    public function can_get_pieces()
+    {
+    }
+}
