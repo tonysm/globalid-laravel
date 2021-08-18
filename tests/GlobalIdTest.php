8f7885da3d0172e7514ce28fdd2ec7685088aa34
Tony Messias <tonysm@hey.com>
2021-08-17 21:17:40 -0300

    Adds global id tests

---
+++ tests/GlobalIdTest.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace Tonysm\GlobalId\Tests;
+
+class GlobalIdTest extends TestCase
+{
+    /** @test */
+    public function value_equality()
+    {
+    }
+
+    /** @test */
+    public function invalid_app_name()
+    {
+    }
+
+    /** @test */
+    public function param_parsing()
+    {
+    }
+
+    /** @test */
+    public function find_with_param()
+    {
+    }
+
+    /** @test */
+    public function find()
+    {
+    }
+
+    /** @test */
+    public function find_with_class()
+    {
+    }
+
+    /** @test */
+    public function find_with_class_no_match()
+    {
+    }
+
+    /** @test */
+    public function find_with_subclass()
+    {
+    }
+
+    /** @test */
+    public function find_with_subclass_no_match()
+    {
+    }
+
+    /** @test */
+    public function find_with_module()
+    {
+    }
+
+    /** @test */
+    public function test_with_module_no_match()
+    {
+    }
+
+    /** @test */
+    public function find_with_multiple_class()
+    {
+    }
+
+    /** @test */
+    public function find_with_multiple_class_no_match()
+    {
+    }
+
+    /** @test */
+    public function find_with_multiple_module()
+    {
+    }
+
+    /** @test */
+    public function find_with_multiple_module_no_match()
+    {
+    }
+
+    /** @test */
+    public function to_string()
+    {
+    }
+
+    /** @test */
+    public function to_params()
+    {
+    }
+
+    /** @test */
+    public function to_uri()
+    {
+    }
+
+    /** @test */
+    public function model_id()
+    {
+    }
+
+    /** @test */
+    public function model_name()
+    {
+    }
+
+    /** @test */
+    public function model_class()
+    {
+    }
+
+    /** @test */
+    public function app_option()
+    {
+    }
+
+    /** @test */
+    public function equality()
+    {
+    }
+
+    /** @test */
+    public function create_custom_params()
+    {
+    }
+
+    /** @test */
+    public function parse_custom_param()
+    {
+    }
+}
